1ce6578e681b66a51e7acf28abc07b89723ce6ce
Adrian Tineo <adrian@adriantineo.com>
2023-08-07 15:34:24 +0200

    Add crypto services

---
+++ app/api/services/coin_market_cap.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+import requests
+from typing import Any, cast
+
+load_dotenv()
+
+COIN_MARKET_CAP_API_KEY = os.getenv("COIN_MARKET_CAP_API_KEY")
+
+
+class CoinMarketCap:
+    def __init__(self):
+        self.host = "https://pro-api.coinmarketcap.com/v1/"
+        self.headers = {
+            "Accept": "application/json",
+            "X-CMC_PRO_API_KEY": COIN_MARKET_CAP_API_KEY
+        }
+
+    def get_coin_prices(self) -> list[tuple[str, float]]:
+        url = self.host + "cryptocurrency/listings/latest"
+        url_params: dict[str, Any] = {
+            "limit": 100,
+            "sort": "market_cap",
+            "convert": "USD"
+        }
+        response = requests.get(url, headers=self.headers, params=url_params)
+        data = response.json()["data"]
+        top_crypto_list = map(
+            lambda x: (x["symbol"], x["quote"]["USD"]["price"]),
+            data
+        )
+        return cast(list[tuple[str, float]], top_crypto_list)
---
+++ app/api/services/crypto_compare.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+import requests
+from typing import Any, cast
+
+load_dotenv()
+
+CRYPTO_COMPARE_API_KEY = os.getenv("CRYPTO_COMPARE_API_KEY")
+
+
+class CryptoCompare:
+    def __init__(self):
+        self.host = "https://min-api.cryptocompare.com/"
+        self.headers = {
+            "Accept": "application/json",
+            "Authorization": "Apikey {}".format(CRYPTO_COMPARE_API_KEY)
+        }
+
+    def get_top_crypto_list(self) -> list[str]:
+        url = self.host + "data/top/totalvolfull"
+        url_params: dict[str, Any] = {
+            "limit": 100
+        }
+        response = requests.get(url, headers=self.headers, params=url_params)
+        data = response.json()["Data"]
+        top_crypto_list = map(
+            lambda x: (x["CoinInfo"]["Name"]),
+            data
+        )
+        return cast(list[str], top_crypto_list)
