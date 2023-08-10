dfaa4880a52962957a203d8d4386172dc9958645
Adrian Tineo <adrian@adriantineo.com>
2023-08-10 18:40:17 +0200

    Handel errors

--- app/api/routes/routes.py
+++ app/api/routes/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from models.models import CryptoEntry, OutputFormat
 from datetime import datetime as dt
+from models.errors import InvalidTime, UnavailableTime
 
 router = APIRouter()
 
@@ -34,6 +35,14 @@ def get_user(request: Request, limit: int, datetime: str | None = None, format: 
 
     # Fetch results
     coin_resolver = request.app.coin_resolver
-    results = coin_resolver.fetch_top_coins(limit, timestamp)
+    results: list[CryptoEntry] = []
+    try:
+        results = coin_resolver.fetch_top_coins(limit, timestamp)
+    except InvalidTime as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Input time was invalid.")
+    except UnavailableTime as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No data available for the specified time.")
 
     return output_format.output(results)
