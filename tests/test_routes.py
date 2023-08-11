d9f9f77da442020d676523a99e12782d5b793df5
Adrian Tineo <adrian@adriantineo.com>
2023-08-11 19:49:41 +0200

    Add real test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,6 +7,7 @@ client = TestClient(app)
 
 
 def test_limit_is_rejected_if_out_of_range():
-    assert True
-    # response = client.get("/top_price_list")
-    # assert response.status_code == 400
+    response = client.get("/top_price_list?limit=9")
+    assert response.status_code == 400
+    response = client.get("/top_price_list?limit=101")
+    assert response.status_code == 400
